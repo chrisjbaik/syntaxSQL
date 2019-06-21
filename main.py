bd38511ad0737a8083b0bcea8777646d65a21ab2
Chris Baik <cjbaik@umich.edu>
2019-06-21 15:29:27 -0400

    change test

--- main.py
+++ main.py
@@ -173,12 +173,12 @@ def test(model, db, schemas, n, b, debug, timeout=None):
     while True:
         db_name = raw_input('Database (hit enter for default) > ')
         if not db_name:
-            db_name = 'concert_singer'
+            db_name = 'world_1'
         print('Database: {}'.format(db_name))
 
         nlq = raw_input('NLQ (hit enter for default) > ')
         if not nlq:
-            nlq = ['Find', 'the', 'id', 'of', 'the', 'pet', 'owned', 'by', 'student', 'whose', 'last', 'name', 'is', u'\u2018Smith\u2019', '.']
+            nlq = ['Give', 'the', 'names', 'of', 'the', 'nations', 'that', 'were', 'founded', 'after', '1950', '.']
         print('NLQ: {}'.format(nlq))
 
         old = translate(model, db, schemas, db_name, nlq, n, b, _old=True,
