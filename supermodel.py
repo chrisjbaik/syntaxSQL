170c3b1925f7dd1a2e98dfa5ea38e9820a2edfd1
Chris Baik <cjbaik@umich.edu>
2019-07-05 20:16:15 -0400

    removearg

--- supermodel.py
+++ supermodel.py
@@ -241,7 +241,7 @@ class SuperModel(nn.Module):
             cur_pq = cur.find_protoquery(cur.query.pq, cur.next)
 
             # update join path if needed
-            states, updated = cur.update_join_paths(cur_pq)
+            states, updated = cur.update_join_paths()
 
             if states is None:      # if error
                 continue
